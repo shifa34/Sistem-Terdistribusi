1de9906ecaf9de1870912334795440ae6b3062ff
shifa34 <115514292+shifa34@users.noreply.github.com>
2023-10-21 21:44:07 +0700

    ChatClient.java

--- ST_2111081002/arshifa230929/ChatClient.java
+++ ST_2111081002/arshifa230929/ChatClient.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package arshifa230922;
+package arshifa230929;
 
 import java.net.*;
 import java.io.*;
