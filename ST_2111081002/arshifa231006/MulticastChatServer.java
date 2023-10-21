0834959a527d3f30f48e69dbbd2c7fce6b6f342c
shifa34 <115514292+shifa34@users.noreply.github.com>
2023-10-21 21:50:33 +0700

    MulticastChatServer.java

--- ST_2111081002/arshifa231006/MulticastChatServer.java
+++ ST_2111081002/arshifa231006/MulticastChatServer.java
@@ -2,10 +2,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package arshifa230922;
-import java.io.*;
-import java.net.*;
-import java.util.*;
+package arshifa231006;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -13,50 +20,53 @@ import java.util.*;
  */
 
 public class MulticastChatServer {
-    private static final String MULTICAST_ADDRESS = "230.0.0.1";
-    private static final int MULTICAST_PORT = 12345;
-    private static final int MAX_USERS= 10;
-    
+     private static final String address = "230.0.0.1";
+    private static final int port = 12346;
+    private static final int max_user = 10;
+
     private static Map<String, InetAddress> users = new HashMap<>();
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
         try {
-             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-            MulticastSocket multicastSocket = new MulticastSocket(MULTICAST_PORT);
+            InetAddress group = InetAddress.getByName(address);
+            MulticastSocket multicastSocket = new MulticastSocket(port);
             multicastSocket.joinGroup(group);
-                    
-            System.out.println("Multicast Chat Server is running..");
+            System.out.println("Multicast chat server running...");
             while (true) {
                 byte[] buffer = new byte[1024];
-                DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 multicastSocket.receive(packet);
-                
+
                 String message = new String(packet.getData(), 0, packet.getLength());
-                
                 String[] parts = message.split(":", 2);
-                 if (parts.length == 2) {
-                     String username = parts[0].trim();
-                     String content = parts[1].trim();
-                     if (!users.containsKey(username)) {
-                        if (users.size() >= MAX_USERS) {
-                            System.out.println("User limit reached." + username + " cannot join.");
-                         continue;
-            }
+                if (parts.length == 2) {
+                    String username = parts[0].trim();
+                    String content = parts[1].trim();
+                    if (!users.containsKey(username)) {
+                        if (users.size() >= max_user) {
+                            System.out.println("user penuh. " + username + " tidak bisa join");
+                            continue;
+                        }
                         users.put(username, packet.getAddress());
-                        System.out.println(username + " joined the chat.");
-        }
-                        for (Map.Entry<String, InetAddress> entry : users.entrySet()) {
+                        System.out.println(username + "Join");
+                    }
+                    for (Map.Entry<String, InetAddress> entry : users.entrySet()) {
                         if (!entry.getKey().equals(username)) {
-    
-                        InetAddress userAddress = entry.getValue();
-                           DatagramPacket sendPacket = new DatagramPacket(
-                                   message.getBytes(),message.length(),userAddress,MULTICAST_PORT);
-                        multicastSocket.send(sendPacket);
+                            InetAddress userAddress = entry.getValue();
+                            DatagramPacket sendPacket = new DatagramPacket(
+                                    message.getBytes(),
+                                    message.length(),
+                                    userAddress,
+                                    port
+                            );
+                            multicastSocket.send(sendPacket);
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            // TODO: handle exception
         }
-        } catch (IOException e) {e.printStackTrace();
-        }
+       
     }
 }
