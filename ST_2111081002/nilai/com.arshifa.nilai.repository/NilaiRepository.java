31711c1a9926562be0816553584431e3ce432f52
shifa34 <115514292+shifa34@users.noreply.github.com>
2023-12-26 20:04:05 +0700

    Create NilaiRepository.java

---
+++ ST_2111081002/nilai/com.arshifa.nilai.repository/NilaiRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.shifa.nilai.repository;
+
+import com.shifa.nilai.entity.Nilai;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author WIN - 10
+ */
+@Repository
+public interface NilaiRepository extends JpaRepository<Nilai, Long>{    
+}
